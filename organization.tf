98c110bebe50518c20d6ae3eb5f67b683bbfb24c
Antonio The Admin <antonio.pinturasacme@gmail.com>
2022-04-05 20:33:08 +0200

    feat(organizations): add Infrastructure OU

--- organization.tf
+++ organization.tf
@@ -8,3 +8,14 @@ resource "aws_organizations_organizational_unit" "security" {
   name      = "Security"
   parent_id = aws_organizations_organization.org.roots[0].id
 }
+
+# Infrastructure OU
+resource "aws_organizations_organizational_unit" "infrastructure" {
+  name      = "Infrastructure"
+  parent_id = aws_organizations_organization.org.roots[0].id
+}
+
+resource "aws_organizations_organizational_unit" "shared" {
+  name      = "Shared"
+  parent_id = aws_organizations_organizational_unit.infrastructure.id
+}
--- output.tf
+++ output.tf
@@ -2,6 +2,16 @@ output "org_id" {
   value = aws_organizations_organization.org.id
 }
 
+# Security OU
 output "security_ou_id" {
   value = aws_organizations_organizational_unit.security.id
+}
+
+# Infrastructure OU
+output "infrastructure_ou_id" {
+  value = aws_organizations_organizational_unit.infrastructure.id
+}
+
+output "shared_ou_id" {
+  value = aws_organizations_organizational_unit.shared.id
 }
